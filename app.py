22de16acc0d39b1721508765620e12eea4028109
JMZLM <109211163+JMZLM@users.noreply.github.com>
2024-05-23 09:01:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -79,10 +79,10 @@ def main():
         unsafe_allow_html=True
     )
 
-    if st.session_state.url:
-        url = st.session_state.url
-        if url.endswith("#cfg"):
-            display_cfg_html()
+    nav_selection = st.sidebar.radio("Navigation", ["HOME", "CFG", "PDA", "ABOUT", "MANUAL"])
+
+    if nav_selection == "CFG":
+        display_cfg_html()
 
 
 
